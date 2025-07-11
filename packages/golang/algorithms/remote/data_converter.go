5900cad24cca63e20da29f61271f5c5753b79fbe
Simon Emms <simon@simonemms.com>
2025-07-11 17:36:20 +0000

    feat(golang): create a remote encoding method
    This is a terrible idea and shouldn't be used. But, if you
    want to know why, this will help you answer that question 🔥

---
+++ packages/golang/algorithms/remote/data_converter.go
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2025 Simon Emms <simon@simonemms.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package remote
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"go.temporal.io/api/common/v1"
+	"go.temporal.io/sdk/converter"
+)
+
+// This is a very silly idea. This encrypter connects to a remote
+// Temporal Codec Server and encrypts everything over that. This
+// will be slow.
+//
+// Very slow. So use this as what _CAN_ be done, not what _SHOULD_
+// be done
+//
+// And development. It's (probably) fine for development.
+type remote struct {
+	url    string
+	client *http.Client
+}
+
+func (r *remote) exec(endpoint string, payloads []*common.Payload) ([]*common.Payload, error) {
+	input, err := json.Marshal(common.Payloads{
+		Payloads: payloads,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error converting payload to json: %w", err)
+	}
+
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/%s", r.url, endpoint), bytes.NewBuffer(input))
+	if err != nil {
+		return nil, fmt.Errorf("error creating http request: %w", err)
+	}
+
+	res, err := r.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error calling encoding endpoint: %w", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("invalid data returned")
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading body: %w", err)
+	}
+
+	output := common.Payloads{}
+	if err := json.Unmarshal(body, &output); err != nil {
+		return nil, fmt.Errorf("error converting payloads: %w", err)
+	}
+
+	return output.GetPayloads(), nil
+}
+
+func (r *remote) Decode(payloads []*common.Payload) ([]*common.Payload, error) {
+	return r.exec("decode", payloads)
+}
+
+func (r *remote) Encode(payloads []*common.Payload) ([]*common.Payload, error) {
+	return r.exec("encode", payloads)
+}
+
+func DataConverter(url string) converter.DataConverter {
+	return converter.NewCodecDataConverter(converter.GetDefaultDataConverter(), &remote{
+		url: url,
+		client: &http.Client{
+			Timeout: time.Second * 5,
+		},
+	})
+}
