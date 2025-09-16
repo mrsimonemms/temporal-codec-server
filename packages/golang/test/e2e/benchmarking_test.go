1e5f24c60fac4e4c1c0b79a0428a0abba6b0bf51
Simon Emms <simon@simonemms.com>
2025-09-16 13:50:06 +0100

    chore: add a more complex and larger input
    The complex data is a reasonably sized JSON object.
    The larger input is a load of Shakespeare plays - the
    total size of all Shakespeare is about 5.2MB, so this
    is a subset of around 1.3mb. The data itself isn't
    important, just that we're putting a load of stuff
    through Temporal and the encryption server.

--- packages/golang/test/e2e/benchmarking_test.go
+++ packages/golang/test/e2e/benchmarking_test.go
@@ -18,15 +18,18 @@ package e2e_test
 
 import (
 	"context"
+	_ "embed"
 	"fmt"
 	"os"
 	"testing"
+	"time"
 
-	"github.com/mrsimonemms/temporal-codec-server/examples/golang"
 	"github.com/mrsimonemms/temporal-codec-server/packages/golang/algorithms/aes"
 	"github.com/mrsimonemms/temporal-codec-server/packages/golang/algorithms/remote"
+	"go.temporal.io/sdk/activity"
 	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/worker"
+	"go.temporal.io/sdk/workflow"
 )
 
 type log struct{}
@@ -36,7 +39,34 @@ func (l log) Error(a string, b ...any) {}
 func (l log) Info(a string, b ...any)  {}
 func (l log) Warn(a string, b ...any)  {}
 
-func runner(b *testing.B, name string, opts client.Options) {
+func Workflow(ctx workflow.Context, data any) (any, error) {
+	ao := workflow.ActivityOptions{
+		StartToCloseTimeout: 10 * time.Second,
+	}
+	ctx = workflow.WithActivityOptions(ctx, ao)
+
+	logger := workflow.GetLogger(ctx)
+	logger.Info("Workflow started")
+
+	var result any
+	err := workflow.ExecuteActivity(ctx, Activity, data).Get(ctx, &result)
+	if err != nil {
+		logger.Error("Activity failed.", "Error", err)
+		return "", err
+	}
+
+	logger.Info("Workflow completed.")
+
+	return result, nil
+}
+
+func Activity(ctx context.Context, name any) (any, error) {
+	logger := activity.GetLogger(ctx)
+	logger.Info("Activity")
+	return name, nil
+}
+
+func runner(b *testing.B, name string, opts client.Options, inputData func(name string, i int) any) {
 	b.Helper()
 
 	// ---- setup (not timed) ----
@@ -52,12 +82,12 @@ func runner(b *testing.B, name string, opts client.Options) {
 	defer c.Close()
 
 	// Unique task queue per benchmark name + pid to avoid queue contention
-	tq := fmt.Sprintf("hello-world-%s-%d", name, os.Getpid())
+	tq := fmt.Sprintf("benchmark-%s-%d", name, os.Getpid())
 
 	w := worker.New(c, tq, worker.Options{})
-	// Register BEFORE starting the worker
-	w.RegisterWorkflow(golang.Workflow)
-	w.RegisterActivity(golang.Activity)
+
+	w.RegisterWorkflow(Workflow)
+	w.RegisterActivity(Activity)
 
 	if err := w.Start(); err != nil {
 		b.Fatal(err)
@@ -76,30 +106,108 @@ func runner(b *testing.B, name string, opts client.Options) {
 			TaskQueue: tq,
 		}
 
-		input := fmt.Sprintf("Run %s %d", name, i)
-		we, err := c.ExecuteWorkflow(ctx, workflowOptions, golang.Workflow, input)
+		input := inputData(name, i)
+		we, err := c.ExecuteWorkflow(ctx, workflowOptions, Workflow, input)
 		if err != nil {
 			b.Fatal(err)
 		}
 
-		var result string
+		var result any
 		if err := we.Get(ctx, &result); err != nil {
 			b.Fatal(err)
-		}
-
-		expected := fmt.Sprintf("Hello %s!", input)
-		if result != expected {
-			b.Fatalf("unexpected result: got %q want %q", result, expected)
 		}
 
 		i++
 	}
 }
 
+func simpleData(name string, i int) any {
+	return fmt.Sprintf("Run %s %d", name, i)
+}
+
+//go:embed testdata/shakespeare.txt
+var shakespeareText string
+
+//go:embed testdata/shakespeare100kb.txt
+var shakespeare100kbText string
+
+// Get a load of Shakespeare. It's not important what it is, just
+// that it's something between 1 and 1.5MB to really stretch what
+// the tests are doing. The limit for Temporal is 2MB, but that's on
+// the encrypted data, so we need to leave something for the encryption
+// to take up in case it gets larger, which it almost certainly will.
+func shakespeare(_ string, _ int) any {
+	return shakespeareText
+}
+
+func shakespeare100kb(_ string, _ int) any {
+	return shakespeare100kbText
+}
+
+func complexData(name string, i int) any {
+	type obj struct {
+		SomeString string
+		SomeNumber float64
+		SomeBool   bool
+	}
+
+	type data struct {
+		Name       string
+		ID         int
+		SomeString string
+		SomeNumber float64
+		SomeBool   bool
+		SomeObj    obj
+		SomeArray  []obj
+	}
+
+	return data{
+		Name: name,
+		ID:   i,
+		SomeString: `Hello. This is a delightful string. Not too long, but does the job
+
+Let's put a line break in. Why? Whyever not?`,
+		SomeNumber: 98103984093891093,
+		SomeBool:   true,
+		SomeObj: obj{
+			SomeString: "This is a much shorter string. Harrumble! Hello " + name,
+			SomeNumber: 3.141,
+			SomeBool:   false,
+		},
+		SomeArray: []obj{
+			{
+				SomeString: "String 1",
+				SomeNumber: 12345,
+				SomeBool:   true,
+			},
+			{
+				SomeString: "String 2",
+				SomeNumber: 2468,
+				SomeBool:   false,
+			},
+		},
+	}
+}
+
 // Run with no data conversion. This should be fastest
 func BenchmarkNoConversion(b *testing.B) {
 	b.ReportAllocs()
-	runner(b, "NoConversion", client.Options{})
+	runner(b, "NoConversion", client.Options{}, simpleData)
+}
+
+func BenchmarkNoConversionComplexData(b *testing.B) {
+	b.ReportAllocs()
+	runner(b, "NoConversionComplexData", client.Options{}, complexData)
+}
+
+func BenchmarkNoConversionShakespeare100kb(b *testing.B) {
+	b.ReportAllocs()
+	runner(b, "NoConversionShakespeare100kb", client.Options{}, shakespeare100kb)
+}
+
+func BenchmarkNoConversionShakespeare(b *testing.B) {
+	b.ReportAllocs()
+	runner(b, "NoConversionShakespeare", client.Options{}, shakespeare)
 }
 
 // Locally convert via AES.
@@ -110,7 +218,37 @@ func BenchmarkLocalAESConversion(b *testing.B) {
 			{ID: "key0", Key: "passphrasewhichneedstobe32bytes!"},
 			{ID: "key1", Key: "anoldpassphraseinourhistory!!!!!"},
 		}),
-	})
+	}, simpleData)
+}
+
+func BenchmarkLocalAESConversionComplexData(b *testing.B) {
+	b.ReportAllocs()
+	runner(b, "AESConversionComplexData", client.Options{
+		DataConverter: aes.DataConverter(aes.Keys{
+			{ID: "key0", Key: "passphrasewhichneedstobe32bytes!"},
+			{ID: "key1", Key: "anoldpassphraseinourhistory!!!!!"},
+		}),
+	}, complexData)
+}
+
+func BenchmarkLocalAESConversionShakespeare100kb(b *testing.B) {
+	b.ReportAllocs()
+	runner(b, "AESConversionShakespeare100kb", client.Options{
+		DataConverter: aes.DataConverter(aes.Keys{
+			{ID: "key0", Key: "passphrasewhichneedstobe32bytes!"},
+			{ID: "key1", Key: "anoldpassphraseinourhistory!!!!!"},
+		}),
+	}, shakespeare100kb)
+}
+
+func BenchmarkLocalAESConversionShakespeare(b *testing.B) {
+	b.ReportAllocs()
+	runner(b, "AESConversionShakespeare", client.Options{
+		DataConverter: aes.DataConverter(aes.Keys{
+			{ID: "key0", Key: "passphrasewhichneedstobe32bytes!"},
+			{ID: "key1", Key: "anoldpassphraseinourhistory!!!!!"},
+		}),
+	}, shakespeare)
 }
 
 // Remotely (via HTTP) convert via AES. It's the same algorithm as above, but
@@ -119,5 +257,26 @@ func BenchmarkRemoteConversion(b *testing.B) {
 	b.ReportAllocs()
 	runner(b, "RemoteConversion", client.Options{
 		DataConverter: remote.DataConverter(os.Getenv("CODEC_SERVER_ADDRESS")),
-	})
+	}, simpleData)
+}
+
+func BenchmarkRemoteConversionComplexData(b *testing.B) {
+	b.ReportAllocs()
+	runner(b, "RemoteConversionComplexData", client.Options{
+		DataConverter: remote.DataConverter(os.Getenv("CODEC_SERVER_ADDRESS")),
+	}, complexData)
+}
+
+func BenchmarkRemoteConversionShakespeare100kb(b *testing.B) {
+	b.ReportAllocs()
+	runner(b, "RemoteConversionShakespeare100kb", client.Options{
+		DataConverter: remote.DataConverter(os.Getenv("CODEC_SERVER_ADDRESS")),
+	}, shakespeare100kb)
+}
+
+func BenchmarkRemoteConversionShakespeare(b *testing.B) {
+	b.ReportAllocs()
+	runner(b, "RemoteConversionShakespeare", client.Options{
+		DataConverter: remote.DataConverter(os.Getenv("CODEC_SERVER_ADDRESS")),
+	}, shakespeare)
 }
